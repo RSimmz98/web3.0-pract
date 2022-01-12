2b713c89f6e012b7e85563520c6413108b328d12
unknown <richsonsimbabure@gmail.com>
2022-01-12 16:16:10 -0500

    adde the adresses of my metamask and updated the smart_contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,9 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/x82tLCNGxE7lAhRwzdMH3AY_Ga6ak0gW',
+      accounts: ['d31950fadd016cfe68ae273b62037061bba9a14a9b6830e8a340ed08cb51fb27'],
     },
   },
 };
